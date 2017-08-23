434fb710446c0d39bbc592d3bf3177dbd0b36c96
Al Chen <atc239@gmail.com>
2017-08-23 00:24:35 -0400

    mapping with structs

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,12 @@
 var ConvertLib = artifacts.require("./ConvertLib.sol");
 var MetaCoin = artifacts.require("./MetaCoin.sol");
 var People = artifacts.require("./People.sol");
+var FernCoin = artifacts.require("./FernCoin.sol");
+
 
 module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
   deployer.link(ConvertLib, MetaCoin);
   deployer.deploy(People);
+  deployer.deploy(FernCoin);
 };
